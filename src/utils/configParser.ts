f3213d455d91efebc3de1fac946d8f5296e521b3
sam bacha <sam@manifoldfinance.com>
2021-10-05 04:20:21 -0700

    refactor(lib): web3-rpc-failover
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

---
+++ src/utils/configParser.ts
@@ -0,0 +1,18 @@
+/**
+ * @file configParser
+ */
+import * as fs from 'fs';
+import * as yaml from 'yaml';
+
+/**
+ * @const getConfig
+ * @summary Get and Parse the Config from the root directory
+ */
+
+const getConfig = (configPath: any) => {
+  const configFile = fs.readFileSync(configPath, 'utf8');
+  return yaml.parse(configFile);
+};
+
+export default getConfig;
+/** @exports getConfig */
